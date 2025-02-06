daa32a70cd670bd9ffc7ea13d5578fb91565d4d6
fatenke <kerroufaten729@gmail.com>
2025-02-06 21:32:24 +0100

    Initial commit for dev branch

--- src/main/java/util/MyConnection.java
+++ src/main/java/util/MyConnection.java
@@ -1,29 +1,43 @@
 package util;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyConnection {
-    final String URL = "jdbc";
+    final String URL = "jdbc:mysql://localhost:3306/hackify";
     final String USER = "root";
     final String PASS ="";
 
     Connection cnx;
     static MyConnection instance;
+
+
+
     //privatisation de constructeur
-    private MyConnection(){
+    private MyConnection (){
         try{
             cnx = DriverManager.getConnection(URL, USER, PASS);
             System.out.println("Connected ");
 
         }catch (SQLException e){
-
-        }
-        public static MyConnection getInstance(){
-            if
-
+            e.printStackTrace();
         }
 
     }
+
+        public static MyConnection getInstance() {
+            if (instance == null) {
+                instance = new MyConnection();
+            }
+            return instance;
+        }
+        public Connection getCnx() {
+            return cnx;
+        }
+
+
+
+
 }
