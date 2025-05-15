5b14b56278bfc7612d844b9776e37b2c75bdb916
fatenke <kerroufaten729@gmail.com>
2025-05-15 15:54:49 +0200

    commit

--- src/main/java/gui/HackathonDetails.java
+++ src/main/java/gui/HackathonDetails.java
@@ -2,19 +2,27 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import models.Hackathon;
 import models.Participation;
 import models.User;
+import services.HackathonService;
 import services.ParticipationService;
 
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 public class HackathonDetails {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm");
     ParticipationService participationService=new ParticipationService();
+    HackathonService hackathonService=new HackathonService();
     @FXML
     private Label labelDescription;
 
@@ -61,6 +69,63 @@ public class HackathonDetails {
         alert.showAndWait();
 
     }
+
+    @FXML
+    void ouvrirUpdateHackathon(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/UpdateHackathon.fxml"));
+            Parent root = loader.load();
+            UpdateHackathon hackathonToUpdate = loader.getController();
+            hackathonToUpdate.setHackathon(hackathon);
+            Stage stage = (Stage) labelDescription.getScene().getWindow();
+            stage.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    void supprimerHackathon(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation de Suppression");
+        alert.setHeaderText(null);
+        alert.setContentText("Voulez-vous vraiment supprimer le hackathon : " + hackathon.getNom_hackathon() + " ?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            participationService.deleteByHackathonId(hackathon.getId_hackathon());
+            hackathonService.delete(hackathon);
+            System.out.println("Hackathon supprimé");
+
+            // Retour à la vue principale (ex: liste des hackathons)
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/AfficherHachathon.fxml"));
+                Parent root = loader.load();
+                Stage stage = (Stage) labelDescription.getScene().getWindow();
+                stage.getScene().setRoot(root);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Suppression annulée");
+        }
+    }
+    @FXML
+    void afficherParticipants(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/AfficherParticipation.fxml"));
+            Parent root = loader.load();
+            AfficherParticipation voirParticipants = loader.getController();
+            voirParticipants.setHackathon(hackathon);
+            Stage stage = (Stage) labelDescription.getScene().getWindow();
+            stage.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 
